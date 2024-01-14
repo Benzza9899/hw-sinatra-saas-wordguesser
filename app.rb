d91064f2d3fa7eb6ce80545b026f8aadd48e56b3
MyChayak <mind69317@gmail.com>
2024-01-14 13:45:18 +0000

    EditingVer

--- app.rb
+++ app.rb
@@ -38,8 +38,15 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    letter = params[:guess].to_s[0]
+    begin
+      if ! @game.guess(letter[0])
+        flash[:message] = "You have already used that letter." 
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,17 +57,22 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    status = @game.check_win_or_lose
+    redirect '/win' if status == :win
+    redirect '/lose' if status == :lose
+    #erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    redirect '/show' unless @game.check_win_or_lose == :win
+    #erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    redirect '/show' unless @game.check_win_or_lose == :lose
+    #erb :lose # You may change/remove this line
   end
   
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,5 +1,5 @@
 class WordGuesserGame
-
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -7,6 +7,38 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    if letter.nil? || letter !~ /\A[a-zA-Z]\Z/i # match single letter,ignoring case
+      raise ArgumentError
+    end
+    letter = letter[0].downcase
+    if  @word.include?(letter) && !@guesses.include?(letter)
+      @guesses += letter
+    elsif !@word.include?(letter) && !@wrong_guesses.include?(letter)
+      @wrong_guesses += letter
+    else
+      false
+    end
+  end
+
+  def word_with_guesses
+    @word.gsub(/./)  { |letter| @guesses.include?(letter) ? letter : '-' }
+  end
+
+  def check_win_or_lose
+    return :play if @word.blank?
+
+    if word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length > 6
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
